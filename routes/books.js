3714a75d1ecbb93036621134f87d4b67a0c62641
KagE-Akumaa <mukuljogi18@gmail.com>
2026-03-30 17:08:46 +0530

    fix the database issue

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,14 @@
-import express from "express";
-import pool from "../db.js";
+import express from 'express';
+import pool from '../db.js';
 
-import auth from "../middlewares/auth.js";
-import requireRole from "../middlewares/authRole.js";
+import auth from '../middlewares/auth.js';
+import requireRole from '../middlewares/authRole.js';
 const booksRouter = express.Router();
 
 // Route to get all the books from the books table from the database
-booksRouter.get("/", async (req, res) => {
+booksRouter.get('/', async (req, res) => {
   try {
-    const sqlQuery = "SELECT * FROM books";
+    const sqlQuery = 'SELECT * FROM books';
     // pool.query() has a row field there is the data for the query
     const result = await pool.query(sqlQuery);
     return res.json(result.rows);
@@ -18,12 +18,12 @@ booksRouter.get("/", async (req, res) => {
 });
 
 // Route to get a single book based on the id
-booksRouter.get("/:id", async (req, res) => {
+booksRouter.get('/:id', async (req, res) => {
   try {
     const id = Number(req.params.id);
     // Check the id
     if (!id) {
-      return res.status(400).json({ error: "Book id is required!" });
+      return res.status(400).json({ error: 'Book id is required!' });
     }
     const sqlQuery = `SELECT * FROM books WHERE book_id = $1`;
     const result = await pool.query(sqlQuery, [id]);
@@ -34,31 +34,31 @@ booksRouter.get("/:id", async (req, res) => {
 });
 
 // Route to add a new book
-booksRouter.post("/", auth, requireRole("author"), async (req, res) => {
+booksRouter.post('/', auth, requireRole('author'), async (req, res) => {
   try {
     //NOTE: To add a new book what are the data we need from the body ?
     //Ans- title, price, stock, availability, author
     const { title, price, stock, availability, author } = req.body;
     // NEED TO VERIFY THE DATA TOO
     if (!title) {
-      return res.status(400).json({ error: "Book Title is required!" });
+      return res.status(400).json({ error: 'Book Title is required!' });
     }
     if (!price) {
-      return res.status(400).json({ error: "Book Price is required!" });
+      return res.status(400).json({ error: 'Book Price is required!' });
     }
     if (!stock) {
-      return res.status(400).json({ error: "Book stock is required!" });
+      return res.status(400).json({ error: 'Book stock is required!' });
     }
     if (!availability) {
-      return res.status(400).json({ error: "Book availability is required!" });
+      return res.status(400).json({ error: 'Book availability is required!' });
     }
     if (!author) {
-      return res.status(400).json({ error: "Book author is required!" });
+      return res.status(400).json({ error: 'Book author is required!' });
     }
 
     // Now we need to add the query to add a new book
     const sqlQuery =
-      "INSERT INTO books( book_title, book_price, book_stock, book_availability, author_id) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+      'INSERT INTO books( book_title, book_price, book_stock, book_availability, author_id) VALUES ($1, $2, $3, $4, $5) RETURNING *';
 
     const result = await pool.query(sqlQuery, [
       title,
@@ -74,24 +74,24 @@ booksRouter.post("/", auth, requireRole("author"), async (req, res) => {
 });
 
 // Route to update price and stock
-booksRouter.put("/:id", auth, requireRole("author"), async (req, res) => {
+booksRouter.put('/:id', auth, requireRole('author'), async (req, res) => {
   try {
     const id = Number(req.params.id);
     if (!id) {
-      return res.status(400).json({ error: "Book id is required!" });
+      return res.status(400).json({ error: 'Book id is required!' });
     }
     const { price, stock, availability } = req.body;
     if (!price) {
-      return res.status(400).json({ error: "Book price is required!" });
+      return res.status(400).json({ error: 'Book price is required!' });
     }
     if (!stock) {
-      return res.status(400).json({ error: "Book stock is required!" });
+      return res.status(400).json({ error: 'Book stock is required!' });
     }
     if (availability === undefined) {
-      return res.status(400).json({ error: "Book availability is required!" });
+      return res.status(400).json({ error: 'Book availability is required!' });
     }
     const sqlQuery =
-      "UPDATE books SET book_price = $1, book_stock = $2, book_availability = $3 WHERE book_id = $4 RETURNING *";
+      'UPDATE books SET book_price = $1, book_stock = $2, book_availability = $3 WHERE book_id = $4 RETURNING *';
     const result = await pool.query(sqlQuery, [price, stock, availability, id]);
     return res.status(200).json(result.rows[0]);
   } catch (err) {
@@ -99,14 +99,14 @@ booksRouter.put("/:id", auth, requireRole("author"), async (req, res) => {
   }
 });
 // Route to delete a book based on id
-booksRouter.delete("/:id", auth, requireRole("author"), async (req, res) => {
+booksRouter.delete('/:id', auth, requireRole('author'), async (req, res) => {
   try {
     const id = Number(req.params.id);
     if (!id) {
-      return res.status(400).json({ error: "Book id is required!" });
+      return res.status(400).json({ error: 'Book id is required!' });
     }
-    const sqlQuery = "DELETE FROM books WHERE book_id = $1 RETURNING *";
-    const result = await pool.query(sqlQuery, [id]);
+    const sqlQuery = 'DELETE FROM books WHERE book_id = $1 RETURNING *';
+    await pool.query(sqlQuery, [id]);
     return res.status(204).send();
   } catch (err) {
     return res.status(500).json({ error: err.message });
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,17 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import dotenv from "dotenv";
+import express from 'express';
+import bcrypt from 'bcrypt';
 
-import jwt from "jsonwebtoken";
-import pool from "../db.js";
-
-dotenv.config();
+import jwt from 'jsonwebtoken';
+import pool from '../db.js';
 
 const usersRouter = express.Router();
 
 // get all the users
-usersRouter.get("/", async (_, res) => {
+usersRouter.get('/', async (req, res) => {
+  console.log(process.env.DB_USER);
+  console.log(process.env.DB_NAME);
   try {
-    const sqlQuery = "SELECT * FROM users";
+    const sqlQuery = 'SELECT * FROM users';
     const result = await pool.query(sqlQuery);
     //NOTE: result.rows is full arrray which have all users like rows[0] will have the first and so on
     //so can't destructor it normally need to use a loop and then destructor each entry
@@ -22,22 +21,23 @@ usersRouter.get("/", async (_, res) => {
     });
     return res.status(200).json(users);
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ error: err.message });
   }
 });
 
 // get single user based on the id
-usersRouter.get("/:id", async (req, res) => {
+usersRouter.get('/:id', async (req, res) => {
   try {
     const id = Number(req.params.id);
     if (!id) {
-      return res.status(400).json({ error: "User id is required!" });
+      return res.status(400).json({ error: 'User id is required!' });
     }
 
-    const sqlQuery = "SELECT * FROM users WHERE user_id = $1";
+    const sqlQuery = 'SELECT * FROM users WHERE user_id = $1';
     const result = await pool.query(sqlQuery, [id]);
     if (result.rows.length == 0) {
-      return res.status(404).json({ error: "User not found!" });
+      return res.status(404).json({ error: 'User not found!' });
     }
     // NOTE: we here also giving the object with hashedPassword so create a new object without the hashedPassword
     const { user_password, ...userWithoutPassword } = result.rows[0];
@@ -48,27 +48,27 @@ usersRouter.get("/:id", async (req, res) => {
 });
 
 // create a new user
-usersRouter.post("/", async (req, res) => {
+usersRouter.post('/', async (req, res) => {
   try {
     // The body will have the data - name, email , password, role
     const { name, email, password, role } = req.body;
     if (!name) {
-      return res.status(400).json({ error: "Name is required!" });
+      return res.status(400).json({ error: 'Name is required!' });
     }
     if (!email) {
-      return res.status(400).json({ error: "Email is required!" });
+      return res.status(400).json({ error: 'Email is required!' });
     }
     if (!password) {
-      return res.status(400).json({ error: "Password is required!" });
+      return res.status(400).json({ error: 'Password is required!' });
     }
     if (!role) {
-      return res.status(400).json({ error: "Role is required!" });
+      return res.status(400).json({ error: 'Role is required!' });
     }
 
     // Remember to hash the password using bcrypt
     const hashedPassword = await bcrypt.hash(password, 10);
     const sqlQuery =
-      "INSERT INTO users(user_name, user_email, user_password, user_role) VALUES($1, $2, $3, $4) RETURNING *";
+      'INSERT INTO users(user_name, user_email, user_password, user_role) VALUES($1, $2, $3, $4) RETURNING *';
     const result = await pool.query(sqlQuery, [
       name,
       email,
@@ -81,21 +81,21 @@ usersRouter.post("/", async (req, res) => {
     return res.status(201).json(userWithoutPassword);
   } catch (err) {
     // standard error code for postgres if we violate the UNIQUE constraint
-    if (err.code === "23505") {
-      return res.status(409).json({ error: "Email already exists!" });
+    if (err.code === '23505') {
+      return res.status(409).json({ error: 'Email already exists!' });
     }
     return res.status(500).json({ error: err.message });
   }
 });
 
 // Delete the user by user_id
-usersRouter.delete("/:id", async (req, res) => {
+usersRouter.delete('/:id', async (req, res) => {
   try {
     const id = Number(req.params.id);
     if (!id) {
-      return res.status(400).json({ error: "User id is required!" });
+      return res.status(400).json({ error: 'User id is required!' });
     }
-    const sqlQuery = "DELETE FROM users WHERE user_id = $1";
+    const sqlQuery = 'DELETE FROM users WHERE user_id = $1';
     const result = await pool.query(sqlQuery, [id]);
     return res.status(204).send();
   } catch (err) {
@@ -104,21 +104,21 @@ usersRouter.delete("/:id", async (req, res) => {
 });
 
 // User login Route
-usersRouter.post("/login", async (req, res) => {
+usersRouter.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email) {
-      return res.status(400).json({ error: "Email is required" });
+      return res.status(400).json({ error: 'Email is required' });
     }
     if (!password) {
-      return res.status(400).json({ error: "Password is required" });
+      return res.status(400).json({ error: 'Password is required' });
     }
     // NOTE: For token we need the user_id and user_role so need to query it in db
     // now need to verify if the user with this email and password exists
-    const sqlQuery = "SELECT * FROM users WHERE user_email = $1";
+    const sqlQuery = 'SELECT * FROM users WHERE user_email = $1';
     const result = await pool.query(sqlQuery, [email]);
     if (result.rows.length == 0) {
-      return res.status(400).json({ error: "Email does not exists" });
+      return res.status(400).json({ error: 'Email does not exists' });
     }
     const { user_id, user_password, user_role } = result.rows[0];
 
@@ -126,11 +126,11 @@ usersRouter.post("/login", async (req, res) => {
     const isMatch = await bcrypt.compare(password, user_password);
 
     if (!isMatch) {
-      return res.status(401).json({ error: "Incorrect password!" });
+      return res.status(401).json({ error: 'Incorrect password!' });
     }
 
     const token = jwt.sign({ user_id, user_role }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
+      expiresIn: '1d',
     });
     return res.status(200).json(token);
   } catch (err) {
